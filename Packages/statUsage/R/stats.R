d1c50fb1f92f376723e81a62e6c4ac2539b56baf
Visanu Wanchai <visanuwan@iuams.attlocal.net>
2017-04-03 22:21:37 -0500

    Add plot function to the package

--- Packages/statUsage/R/stats.R
+++ Packages/statUsage/R/stats.R
@@ -47,6 +47,86 @@ stats_geneDNA <- function(dna) {
   return(ret)
 }
 
+  ##### Visualization of Codon/Aa usage #####
+  #
+  # Args:
+  #   Data frame of stats of Codon/Aa usage:
+  #   --------------------------------------
+  #   |   codon_aa   |   type   |   value   |
+  #   --------------------------------------
+  #   |    codon     |    AAA   |  3.35984  |
+  #   |    codon     |    CAA   |  1.58354  |
+  #   |    codon     |    ...   |  .......  |
+  #   |      aa      |     G    |  7.09010  |
+  #   |      aa      |     A    |  9.51250  |
+  #   |      aa      |     .    |  .......  |
+  #   --------------------------------------
+  #
+  # Returns:
+  #   Radar plots
 
+codonUsage <- function(DF_CodonAaUsage) {
 
+  # select a codon column
+  x <- subset(DF_CodonAaUsage[DF_CodonAaUsage$codon_aa == 'codon',], select=c('type','value'))
+  rownames(x) <- 1:nrow(x)
+  x_min <- min(x$value)
+  x_max <- max(x$value)
 
+  # transpose from column to rows *** required by a fmsb package
+  datax=as.data.frame(matrix( x$value , ncol=length(x$value)))
+  colnames(datax) <- x$type
+  datax <- rbind(rep(round(x_max, digits = 2),length(x$value)) ,
+    rep(round(x_min, digits = 2),length(x$value)) , datax)
+
+  # plot a radar chart from fmsb package
+  radarchart( datax  , axistype=1 , seg = 5,
+            
+    #custom polygon
+    pcol=rgb(0.5,0.0,0.0,0.9) , pfcol=rgb(0.5,0.0,0.0,0.5) , plwd=3 , 
+    
+    #custom the grid
+    cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(round(x_min, digits = 2),
+      round(x_max, digits = 2), (round(x_max, digits = 2)-round(x_min, digits = 2))/5), cglwd=0.8,
+    
+    #custom labels
+    vlcex=0.8,
+
+    #title
+    title="Codon usage"
+  )
+
+}
+
+aaUsage <- function(DF_CodonAaUsage) {
+
+  # select an aa colomn
+  x <- subset(DF_CodonAaUsage[DF_CodonAaUsage$codon_aa == 'aa',], select=c('type','value'))
+  rownames(x) <- 1:nrow(x)
+  x_min <- min(x$value)
+  x_max <- max(x$value)
+
+  # transpose from column to rows *** required by a fmsb package
+  datax=as.data.frame(matrix( x$value , ncol=length(x$value)))
+  colnames(datax) <- x$type
+  datax <- rbind(rep(round(x_max, digits = 2),length(x$value)) ,
+    rep(round(x_min, digits = 2),length(x$value)) , datax)
+
+  # plot a radar chart from fmsb package
+  radarchart( datax  , axistype=1 , seg = 5,
+            
+    #custom polygon
+    pcol=rgb(0.0,0.0,0.5,0.9) , pfcol=rgb(0.0,0.0,0.5,0.5) , plwd=3 , 
+    
+    #custom the grid
+    cglcol="grey", cglty=1, axislabcol="grey", caxislabels=seq(round(x_min, digits = 2),
+      round(x_max, digits = 2), (round(x_max, digits = 2)-round(x_min, digits = 2))/5), cglwd=0.8,
+    
+    #custom labels
+    vlcex=1,
+
+    #title
+    title="Amino acid usage"
+  )
+  
+}
